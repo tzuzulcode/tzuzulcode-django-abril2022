037d64d4939e672f2222676d0507bbc36822cf73
Tzuzul <mail@tzuzulcode.com>
2022-06-10 15:09:44 -0500

    Pago completado

--- Ecommerce/cart/urls.py
+++ Ecommerce/cart/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.get_cart, name="cart"),
     path('add/<int:idProduct>', views.add_to_cart, name="add_to_cart"),
     path('change_amount', views.change_amount, name="change_amount"),
-    path('delete/<int:id_cart_item>', views.delete_from_cart, name="delete_from_cart")
+    path('delete/<int:id_cart_item>', views.delete_from_cart, name="delete_from_cart"),
+    path('create_paypal_order', views.create_paypal_order, name="create_paypal_order"),
+    path('capture_paypal_order/<str:order_id>', views.capture_paypal_order, name="capture_paypal_order"),
 ]
--- Ecommerce/cart/views.py
+++ Ecommerce/cart/views.py
@@ -10,6 +10,8 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 paypal_url = "https://api-m.sandbox.paypal.com"
+paypal_client_id = "ARZ3J_aw_cUBiMhrfFa_Tncdp4Eyrblss7_gSaYsU2hcZ_zHocd8MIcheJLn-oYYIa0u4Zm72kFeHU5r"
+paypal_client_secret = "ELpvcVnZA8azlEUUzOhb_jPCAhLGMiX7f3yz7VKlo4Vll0ZrypJM2Z09tmdW0vGSziZAcJ2ptlXqmsWp"
 
 # Create your views here.
 auth_middleware = decorator_from_middleware(AuthMiddleware)
@@ -26,7 +28,9 @@ def get_cart(request):
             total += cartItem.product.price * cartItem.amount
         return render(request, "pages/cart.html", {
             "products": products,
-            "total": total
+            "total": total,
+            "client_id": paypal_client_id,
+            "client_token": generate_client_token()
         })
 
     return redirect("/")
@@ -67,6 +71,7 @@ def change_amount(request):
             "total": total
         })
 
+
 def delete_from_cart(request, id_cart_item):
     if request.user.is_authenticated:
         cart_item = CartItem.objects.get(pk=id_cart_item)
@@ -78,17 +83,77 @@ def delete_from_cart(request, id_cart_item):
     return redirect("/")
 
 
-def get_access_token():
-    return "A21AAJ6ODMptp-ReDHRpKUSHWo1378r_P_cvn2yAuZs1FOxTmtKiQLg6ZbKGa4o6v5oxzR3y2kD457_TxswnCGw7ZubmLF3-g"
+@csrf_exempt
+def create_paypal_order(request):
+    products = request.user.cart.products.all()
+    total = 0
+    for cartItem in products:
+        total += cartItem.product.price * cartItem.amount
 
-def generate_client_token(request):
+    access_token = get_access_token()
+    create_order_url = paypal_url+"/v2/checkout/orders"
+    response = requests.post(create_order_url, headers={
+        "Authorization": "Bearer " + access_token,
+        "Content-Type": "application/json",
+    }, data=json.dumps({
+        "intent": "CAPTURE",
+        "purchase_units": [
+            {
+                "amount": {
+                    "currency_code": "USD",
+                    "value": total
+                }
+            }
+        ]
+    }))
+
+    data = response.json()
+
+    return JsonResponse({
+        "order": data
+    })
+
+
+@csrf_exempt
+def capture_paypal_order(request, order_id):
+    access_token = get_access_token()
+    capture_order_url = paypal_url+"/v2/checkout/orders/"+order_id+"/capture"
+    response = requests.post(capture_order_url, headers={
+        "Authorization": "Bearer " + access_token,
+        "Content-Type": "application/json",
+    })
+
+    data = response.json()
+    print(data)
+    cart_items = request.user.cart.delete()
+    new_cart = Cart()
+    new_cart.user = request.user
+    new_cart.save()
+
+    return JsonResponse(data)
+
+
+def get_access_token():
+    access_token_url = paypal_url + "/v1/oauth2/token"
+    response = requests.post(access_token_url,data={
+        "grant_type": "client_credentials",
+    }, auth=HTTPBasicAuth(paypal_client_id, paypal_client_secret))
+
+    data = response.json()
+
+    return data['access_token']
+
+
+def generate_client_token():
     access_token = get_access_token()
 
-    client_token_url = paypal_url + "/v1/oauth2/token"
+    client_token_url = paypal_url + "/v1/identity/generate-token"
     response = requests.post(client_token_url, headers={
         "Authorization": "Bearer "+access_token,
         "Accept-Language": "en_US",
         "Content-Type": "application/json",
     })
 
-    print(response)
+    data = response.json()
+
+    return data['client_token']
